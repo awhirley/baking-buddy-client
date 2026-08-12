da9b5c21a2e83d029fdcb58ff01f308991a71b5d
Alex Whirley <alexwhirley@gmail.com>
2026-08-12 12:37:28 -0600

    Route create button to create page

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -1,11 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 import { Button } from "#components/ui/button";
 import { H1 } from "#components/ui/typography";
 
 export function Header() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-row gap-x-4 pb-10 flex flex-col justify-between w-200 items-center">
       <H1>Baking Buddy</H1>
-      <Button>New Recipe</Button>
+      <Button onClick={() => navigate("/create")}>New Recipe</Button>
     </div>
   );
 }
