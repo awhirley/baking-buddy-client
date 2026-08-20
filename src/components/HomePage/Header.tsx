bec5fed3d284992778bad193f51fe36652ea527c
Alex Whirley <alexwhirley@gmail.com>
2026-08-20 15:25:36 -0600

    Change button in header depending on location

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -1,15 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { Button } from "#components/ui/button";
 import { H1 } from "#components/ui/typography";
 
 export function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <div className="flex flex-row gap-x-4 pb-10 flex flex-col justify-between w-200 items-center">
       <H1>Baking Buddy</H1>
-      <Button onClick={() => navigate("/create")}>New Recipe</Button>
+      {location.pathname === "/" && <Button onClick={() => navigate("/create")}>New Recipe</Button>}
+      {location.pathname !== "/" && <Button onClick={() => navigate("/")}>Home</Button>}
     </div>
   );
 }
