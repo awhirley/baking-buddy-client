e84bbc79ebf6ebbaee0936084be734873747d6b7
Alex Whirley <alexwhirley@gmail.com>
2026-08-24 10:22:03 -0400

    home page

--- src/components/ui/hero-section-1.tsx
+++ src/components/ui/hero-section-1.tsx
@@ -3,8 +3,8 @@
 import { Button } from "#components/ui/button";
 import { Tagline } from "#components/ui/tagline";
 import { AspectRatio } from "#components/ui/aspect-ratio";
-import Image from "next/image";
 import { ArrowUpRight } from "lucide-react";
+import { H1, P } from "./typography";
 
 export function HeroSection1() {
   return (
@@ -15,21 +15,21 @@ export function HeroSection1() {
       <div className="container-padding-x mx-auto flex max-w-7xl flex-col items-center gap-12 lg:flex-row lg:gap-16">
         <div className="flex flex-1 flex-col gap-6 lg:gap-8">
           <div className="section-title-gap-xl flex flex-col items-center text-center lg:items-start lg:text-left">
-            <Tagline variant="link">
+            {/* <Tagline variant="link">
               <span className="size-1.5 rounded-full bg-green-500" />
               <span className="hidden lg:inline">What's new · </span>
               <span className="lg:text-muted-foreground">
                 Just shipped v2.0
               </span>
               <ArrowUpRight />
-            </Tagline>
-            <h1 id="hero-heading" className="heading-xl">
-              Solve your user's main problem
-            </h1>
-            <p className="text-muted-foreground text-lg/8 text-pretty">
-              Follow with one or two sentences that expand on your value
-              proposition and focus on key benefits.
-            </p>
+            </Tagline> */}
+            <H1 className="heading-xl">
+              Track your recipe adjustments and bake results
+            </H1>
+            <P className="text-muted-foreground text-lg/8 text-pretty">
+              See ingredient and instruction history, take notes,  & rate and upload pictures of your bakes.
+              Organize and tag your recipes to find easily later on.
+            </P>
           </div>
           <div className="flex items-center justify-center gap-2 lg:justify-start">
             <Button>Get started</Button>
@@ -39,13 +39,7 @@ export function HeroSection1() {
 
         <div className="w-full flex-1">
           <AspectRatio ratio={1 / 1}>
-            <Image
-              src="https://ui.shadcn.com/placeholder.svg"
-              alt="Hero section visual"
-              fill
-              priority
-              className="h-full w-full rounded-xl object-cover"
-            />
+            <img src="../../assets/oven.png" />
           </AspectRatio>
         </div>
       </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import { Body } from "#components/HomePage/Body";
 import { Header } from "#components/HomePage/Header";
+import { HeroSection1 } from "#components/ui/hero-section-1";
 
 export function HomePage() {
   return (
     <div className="px-30 py-20">
       <div className="grid grid-cols-1 gap-4 justify-items-center">
         <Header />
+        <HeroSection1 />
         <Body />
       </div>
     </div>
