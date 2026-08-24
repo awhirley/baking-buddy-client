5ec2194be1a689b43b4dc043f2978dcd8af39ba8
Alex Whirley <alexwhirley@gmail.com>
2026-08-24 10:03:51 -0400

    Header buttons

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -11,7 +11,11 @@ export function Header() {
     <div className="flex flex-row gap-x-4 pb-10 flex flex-col justify-between w-200 items-center">
       <H1>Baking Buddy</H1>
       {location.pathname === "/" && <Button onClick={() => navigate("/create")}>New Recipe</Button>}
-      {location.pathname !== "/" && <Button onClick={() => navigate("/")}>Home</Button>}
+      {location.pathname.includes("create")&& <Button onClick={() => navigate("/")}>Home</Button>}
+      {location.pathname.includes("view") && <div className="flex gap-x-2">
+        <Button variant="outline" onClick={() => console.log("new bake")}>New Bake</Button>
+        <Button onClick={() => navigate("/")}>Home</Button>
+      </div>}
     </div>
   );
 }
