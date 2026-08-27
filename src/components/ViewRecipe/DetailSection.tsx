e286930baebd30282ce9e289a588f93b480e7526
Alex Whirley <alexwhirley@gmail.com>
2026-08-27 17:27:49 -0600

    edit mode toggle

--- src/components/ViewRecipe/DetailSection.tsx
+++ src/components/ViewRecipe/DetailSection.tsx
@@ -3,33 +3,30 @@ import { Badge } from "#components/SharedComponents/ui/badge";
 import { Button } from "#components/SharedComponents/ui/button";
 import { Textarea } from "#components/ui/textarea"
 import { Card, CardContent, CardHeader, CardTitle } from "#components/SharedComponents/ui/card";
-import { Pencil, PencilSparklesIcon } from "lucide-react";
-import { useState } from "react";
+import { NotepadText, Pencil, PencilSparklesIcon } from "lucide-react";
+import { useState, type Dispatch, type SetStateAction } from "react";
 import type { RecipeDetail } from "../../types/RecipeTypes";
 import { formatAddedDate } from "#components/RecipeList/utils";
 import { useMutation } from "@tanstack/react-query";
 import { recipeService } from "../../services/RecipeService";
 import { useToast } from "../../contexts/ToastContext";
 import { Spinner } from "#components/SharedComponents/ui/spinner";
+import { Toggle } from "#components/ui/toggle";
 
-export function RecipeDetailsCard({
-  id,
-  name,
-  description,
-  recipeSourceType,
-  recipeSource,
-  tags,
-  tools,
-  createdAt,
-  notes,
-}: RecipeDetail) {
+interface RecipeDetailsCardProps {
+  details: RecipeDetail,
+  editModeOn: boolean,
+  setEditModeOn: Dispatch<SetStateAction<boolean>>,
+}
+
+export function RecipeDetailsCard({ details, editModeOn, setEditModeOn }: RecipeDetailsCardProps) {
   const [deleteDialogIsOpen, setDeleteDialogIsOpen] = useState(false);
-  const [note, setNote] = useState<string | null>(notes);
+  const [note, setNote] = useState<string | null>(details.notes);
   const { addToast } = useToast();
 
   const { mutate: saveNote, isPending: isSaving } = useMutation({
     mutationFn: (note: string | null) =>
-      recipeService.addNotesToRecipe(id, note),
+      recipeService.addNotesToRecipe(details.id, note),
     onSuccess: () => {
       addToast('Note saved successfully', null, { type: 'default' });
     },
@@ -42,38 +39,39 @@ export function RecipeDetailsCard({
     <Card>
       <CardHeader className="flex flex-row items-start justify-between gap-4">
         <div>
-          <CardTitle className="text-2xl">{name}</CardTitle>
+          <CardTitle className="text-2xl">{details.name}</CardTitle>
           <p className="text-sm text-muted-foreground mt-1">
-            Added {formatAddedDate(createdAt)}
-            { (recipeSourceType || recipeSource) && <span>, from {recipeSourceType} {recipeSource}</span>}
+            Added {formatAddedDate(details.createdAt)}
+            { (details.recipeSourceType || details.recipeSource) && <span>, from {details.recipeSourceType} {details.recipeSource}</span>}
           </p>
         </div>
         <div className="flex gap-2">
-          <Button variant="outline" size="icon" aria-label="Edit recipe details">
-            <Pencil className="h-4 w-4" />
-          </Button>
-          <DeleteRecipeTrigger recipeId={id} isOpen={deleteDialogIsOpen} setIsOpen={setDeleteDialogIsOpen} navigateToHome={true} />
+          <Toggle aria-label="Toggle bookmark" variant="outline" pressed={editModeOn} onPressedChange={setEditModeOn} >
+            <Pencil className="group-aria-pressed/toggle:fill-foreground"/>
+            <span className="pl-1">Edit Mode</span>
+          </Toggle>
+          <DeleteRecipeTrigger recipeId={details.id} isOpen={deleteDialogIsOpen} setIsOpen={setDeleteDialogIsOpen} navigateToHome={true} />
         </div>
       </CardHeader>
       <CardContent className="flex flex-col gap-6">
-        <p className="text-sm">{description}</p>
+        <p className="text-sm">{details.description}</p>
 
-        {(tags?.length > 0 || tools?.length > 0) && (
+        {(details.tags?.length > 0 || details.tools?.length > 0) && (
           <div className="flex flex-col gap-2">
-            {tags.length > 0 && (
+            {details.tags.length > 0 && (
               <div className="flex flex-wrap gap-2">
                 Tags:
-                {tags.map((tag) => (
+                {details.tags.map((tag) => (
                   <Badge key={tag} variant="secondary">
                     {tag}
                   </Badge>
                 ))}
               </div>
             )}
-            {tools.length > 0 && (
+            {details.tools.length > 0 && (
               <div className="flex flex-wrap gap-2">
                 Tools:
-                {tools.map((tool) => (
+                {details.tools.map((tool) => (
                   <Badge key={tool} variant="outline">
                     {tool}
                   </Badge>
@@ -83,15 +81,26 @@ export function RecipeDetailsCard({
           </div>
         )}
 
-        <div className="flex flex-col gap-2 group">
-          <Textarea value={note ?? undefined} onChange={(event) => { setNote(event.target.value) }} className="border p-2" placeholder="Recipe notes: enter anything you'd like to remember about your recipe"></Textarea>
-          <Button onClick={() => saveNote(note)} className="hidden group-focus-within:block self-end">
-            <span className="flex items-center gap-2">
-              { isSaving ? <Spinner /> : <PencilSparklesIcon /> }
-              Save note
-            </span>
-          </Button>
-        </div>
+        {
+          editModeOn && <div className="flex flex-col gap-2 group">
+            <span className="flex items-center gap-1 font-semibold"><NotepadText className="w-4 h-4"/>Recipe Notes</span>
+            <Textarea value={note ?? undefined} onChange={(event) => { setNote(event.target.value) }} className="border" placeholder="Recipe notes: enter anything you'd like to remember about your recipe"></Textarea>
+            <Button onClick={() => saveNote(note)} className="hidden group-focus-within:block self-end">
+              <span className="flex items-center gap-2">
+                { isSaving ? <Spinner /> : <PencilSparklesIcon /> }
+                Save note
+              </span>
+            </Button>
+          </div>
+        }
+
+        {
+          (!editModeOn && note) &&
+          <>
+            <span className="flex items-center gap-1 font-semibold"><NotepadText className="w-4 h-4"/>Recipe Notes</span>
+            {note}
+          </>
+        }
       </CardContent>
     </Card>
   );
