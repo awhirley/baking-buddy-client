d1679c5674fc4ccef1f159285e6d5d3ce653f017
Alex Whirley <alexwhirley@gmail.com>
2026-09-01 13:08:22 -0600

    Allow alert to be hidden

--- src/components/Bakes/BakeView.tsx
+++ src/components/Bakes/BakeView.tsx
@@ -1,16 +1,19 @@
 import { useParams } from "react-router-dom";
-import { AlertCircle, Info } from "lucide-react";
+import { AlertCircle, Info, X } from "lucide-react";
 import { skipToken, useQuery } from "@tanstack/react-query";
 
-import { Alert, AlertDescription, AlertTitle } from "#components/SharedComponents/ui/alert";
+import { Alert, AlertAction, AlertDescription, AlertTitle } from "#components/SharedComponents/ui/alert";
 import { bakeService } from "../../services/BakeService";
 import { BakeSkeleton } from "./BakeSkeleton";
 import { BakeDetailsCard } from "./BakeDetailsCard";
 import { BakeIngredientsSection } from "./BakeIngredientsSection";
 import { BakeInstructionsSection } from "./BakeInstructionsSection";
+import { Button } from "#components/SharedComponents/ui/button";
+import { useState } from "react";
 
 export function BakeView() {
   const { bakeId } = useParams();
+  const [alertIsOpen, setAlertIsOpen] = useState(true);
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["bake", "new", bakeId],
@@ -38,13 +41,18 @@ export function BakeView() {
 
       {data && (
         <div className="flex flex-col gap-6">
-          { !data.details.endDatetime && <Alert>
+          { (!data.details.endDatetime && alertIsOpen) && <Alert>
             <Info className="h-4 w-4" />
             <AlertTitle>You're in an active bake session</AlertTitle>
             <AlertDescription>
               This is a working copy of the recipe for this bake. Edits you make to ingredients or
-              instructions here only apply to this session — the original recipe stays unchanged.
+              instructions here only apply to this session until you complete the bake — the original recipe stays unchanged.
             </AlertDescription>
+            <AlertAction>
+              <Button size="icon" variant="ghost" onClick={() => setAlertIsOpen(false)}>
+                <X className="h-4 w-4" />
+              </Button>
+            </AlertAction>
           </Alert>}
 
           <BakeDetailsCard {...data.details} />
